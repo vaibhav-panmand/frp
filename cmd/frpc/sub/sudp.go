573daeae79b938f670fa0b83ee727b23833044aa
vaibhav-panmand <35999552+vaibhav-panmand@users.noreply.github.com>
2021-01-13 17:59:24 +0530

    Update sudp.go

--- cmd/frpc/sub/sudp.go
+++ cmd/frpc/sub/sudp.go
@@ -18,8 +18,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/fatedier/frp/pkg/config"
-	"github.com/fatedier/frp/pkg/consts"
+	"github.com/vaibhav-panmand/frp/pkg/config"
+	"github.com/vaibhav-panmand/frp/pkg/consts"
 
 	"github.com/spf13/cobra"
 )
