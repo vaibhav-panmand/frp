50b582330846099d4293bbc18f777407ac1dcfc2
vaibhav-panmand <35999552+vaibhav-panmand@users.noreply.github.com>
2021-01-13 18:07:56 +0530

    Update stcp.go

--- cmd/frpc/sub/stcp.go
+++ cmd/frpc/sub/stcp.go
@@ -18,8 +18,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/fatedier/frp/pkg/config"
-	"github.com/fatedier/frp/pkg/consts"
+	"github.com/fatedier/vaibhav-panmand/pkg/config"
+	"github.com/fatedier/vaibhav-panmand/pkg/consts"
 
 	"github.com/spf13/cobra"
 )
