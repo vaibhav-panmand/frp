3c805668f1dfff6ed6af71b050bb0085f209baed
vaibhav-panmand <35999552+vaibhav-panmand@users.noreply.github.com>
2021-01-13 18:02:20 +0530

    Update http.go

--- cmd/frpc/sub/http.go
+++ cmd/frpc/sub/http.go
@@ -19,8 +19,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/fatedier/frp/pkg/config"
-	"github.com/fatedier/frp/pkg/consts"
+	"github.com/vaibhav-panmand/frp/pkg/config"
+	"github.com/vaibhav-panmand/frp/pkg/consts"
 
 	"github.com/spf13/cobra"
 )
